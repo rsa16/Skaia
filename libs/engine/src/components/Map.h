57e68efb2954f3bfe18c759cc6eb47aa86371215
rsa16 <sipnet66@outlook.com>
2023-08-23 23:11:12 -0400

    add hierarchy and map component

---
+++ libs/engine/src/components/Map.h
@@ -0,0 +1,27 @@
+/*
+  Copyright (c) 2022, Rehan Ali
+  All rights reserved.
+
+  This source code is licensed under the BSD-style license found in the
+  LICENSE file in the root directory of this source tree.
+*/
+
+#pragma once
+#include "SkaiaCore.h"
+
+#ifdef _DEBUG
+	#undef main
+#endif
+
+namespace Skaia
+{
+    namespace Components
+    {
+        struct ENGINE_API Map {
+            int tileSize;
+
+            std::unordered_map<std::string, std::string> tilesetPaths;
+            std::string mapPath;
+        };
+    }
+}
